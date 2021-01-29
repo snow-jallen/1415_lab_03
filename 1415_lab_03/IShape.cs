da533ec2da427aeb0f10577f22144e13a43feaf3
Jonathan Allen <jallen@snow.edu>
2021-01-29 08:10:43 -0700

    Add project files.

---
+++ 1415_lab_03/IShape.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Media;
+
+namespace _1415_lab_03
+{
+    public interface IShape
+    {
+        public Point Origin { get; init; }
+        public Brush Fill { get; init; }
+        public Brush Stroke { get; init; }
+        public int StrokeThickness { get; init; }
+        public decimal CalculateArea();
+    }
+
+    public class Square : IShape
+    {
+        public Square()
+        {
+        }
+
+        public Square(Point origin, int sideLength)
+        {
+            Origin = origin;
+            SideLength = sideLength;
+        }
+        public int SideLength { get; init; }
+        public Point Origin { get; init; }
+        public Brush Fill { get; init; }
+        public Brush Stroke { get; init; }
+        public int StrokeThickness { get; init; }
+
+        public decimal CalculateArea()
+        {
+            return SideLength * SideLength;
+        }
+    }
+}
---
+++ 1415_lab_03/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _1415_lab_03
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Loaded += MainWindow_Loaded;
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            var squares = new[]
+            {
+                new Square(new Point(50,-50), 100){Stroke = Brushes.Black, Fill = Brushes.AliceBlue },
+                new Square(new Point(250,250), 70){Stroke = Brushes.Yellow, Fill=Brushes.BlueViolet },
+            };
+
+            foreach(var s in squares)
+            {
+                canvas.Children.Add(new Rectangle
+                {
+                    Stroke = s.Stroke,
+                    Fill = s.Fill,
+                    Height=s.SideLength,
+                    Width=s.SideLength,
+                    
+                });
+
+            }
+        }
+    }
+}
