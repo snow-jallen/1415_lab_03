2082c8c22394107143a7490b0151ca408550f83c
Jonathan Allen <jallen@snow.edu>
2021-02-01 10:03:45 -0700

    Redacted version for student use

--- 1415_lab_03/ShapeGenerator.cs
+++ 1415_lab_03/ShapeGenerator.cs
@@ -20,29 +20,13 @@ namespace _1415_lab_03
                     Stroke = Brushes.Violet,
                     StrokeThickness = 10,
                 },
-                new Square(new Point{ X = 100, Y = 100}, 20)
-                {
-                    Fill = Brushes.Blue,
-                    Stroke = Brushes.DarkSalmon,
-                    StrokeThickness = 15,
-                },
                 new Ellipse(new Point { X = 150, Y = 150}, 20, 35)
                 {
                     Fill = Brushes.Red,
                     Stroke = Brushes.Honeydew,
                     StrokeThickness = 15
                 },
-                new Rectangle(new Point {X = 200, Y = 200}, 35, 25)
-                {
-                    Fill = Brushes.LightCyan,
-                    Stroke = Brushes.Indigo,
-                    StrokeThickness = 20
-                },
-                new Triangle(new Point {X = 350, Y = 350}, 20, 15)
-                {
-                    Fill = Brushes.DarkSlateBlue,
-                    Stroke = Brushes.HotPink,
-                }
+                //Add your other shapes here
             };
         }
     }
--- 1415_lab_03/Shapes/IShape.cs
+++ 1415_lab_03/Shapes/IShape.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Text;
 using System.Windows;
 using System.Windows.Media;
@@ -13,90 +12,5 @@ namespace _1415_lab_03.Shapes
         public Brush Stroke { get; init; }
         public int StrokeThickness { get; init; }
         public decimal CalculateArea();
-    }
-
-    public abstract class Shape : IShape
-    {
-        public Shape(Point origin)
-        {
-            Origin = origin;
-        }
-
-        public Point Origin { get; init; }
-        public Brush Fill { get; init; }
-        public Brush Stroke { get; init; }
-        public int StrokeThickness { get; init; }
-        public abstract decimal CalculateArea();
-    }
-
-    public class Square : Rectangle
-    {
-        public Square(Point origin, int sideLength)
-            : base(origin, sideLength, sideLength)
-        {
-        }
-    }
-
-    public class Rectangle : Shape
-    {
-        public Rectangle(Point origin, int length, int width)
-            : base(origin)
-        {
-            Height = length;
-            Width = width;
-        }
-
-        public int Height { get; init; }
-        public int Width { get; init; }
-
-        public override decimal CalculateArea()
-        {
-            return Height * Width;
-        }
-    }
-
-    public class Ellipse : Shape
-    {
-        public Ellipse(Point origin, int radius1, int radius2)
-            : base(origin)
-        {
-            Radius1 = radius1;
-            Radius2 = radius2;
-        }
-
-        public int Radius1 { get; init; }
-        public int Radius2 { get; init; }
-
-        public override decimal CalculateArea()
-        {
-            return (decimal)(Math.PI * Radius1 * Radius2);
-        }
-    }
-
-    public class Circle : Ellipse
-    {
-        public Circle(Point origin, int radius)
-            : base(origin, radius, radius)
-        {
-
-        }
-    }
-
-    public class Triangle : Shape
-    {
-        public Triangle(Point origin, int baseLength, int heightLength)
-            : base(origin)
-        {
-            Base = baseLength;
-            Height = heightLength;
-        }
-
-        public int Base { get; init; }
-        public int Height { get; init; }
-
-        public override decimal CalculateArea()
-        {
-            return .5M * Base * Height;
-        }
     }
 }
